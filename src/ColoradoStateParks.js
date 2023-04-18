6d10665a99233dd96b00f32be2bcdcb4ccf2e1d1
Michal <khakaimic4@gmail.com>
2023-04-19 02:37:05 +0300

    completed code along

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { trees, wildlife } from "./parks/RockyMountain";
+
+console.log(trees);
+// => "Aspen and Pine"
+wildlife();
+// => "Elk, Bighorn Sheep, Moose"
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <div>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+    </div>
+  );
 }
+
+export default ColoradoStateParks;
